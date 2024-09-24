f8a7744adbea8e2748c04afebc25fe0fa59965d5
Florian Geier <florian.geier@unibas.ch>
2024-09-24 21:29:53 +0200

    Update package description and docs

--- R/JohnsonKinaseData-package.R
+++ R/JohnsonKinaseData-package.R
@@ -1,31 +1,6 @@
-#' JohnsonKinaseData
-#'
-#' @description The \pkg{JohnsonKinaseData} package provides position specific
-#'   weight matrices (PWMs) for 303 human serine/threonine and 93 tyrosine
-#'   kinases originally published in Johnson et al. 2023. and Yaron-Barir et a.
-#'   2024
-#'
-#'   It also includes pre-computed PWM scores ("background scores") for a large
-#'   collection of curated human phosphosites.
-#'
-#'   The package additionally offers functionality to match kinase PWMs against
-#'   user-provided phospho-peptides and to rank PWM scores relative to the
-#'   background scores ("percentile rank").
-#'
-#' @references Johnson, J.L., Yaron, T.M., Huntsman, E.M. et al. An atlas of
-#'   substrate specificities for the human serine/threonine kinome. Nature 613,
-#'   759–766 (2023). https://doi.org/10.1038/s41586-022-05575-3
-#'
-#'   Yaron-Barir, T.M., Joughin, B.A., Huntsman, E.M. et al. The intrinsic
-#'   substrate specificity of the human tyrosine kinome. Nature 629, 1174–1181
-#'   (2024). https://doi.org/10.1038/s41586-024-07407-y
-#'
-#'   Yaffe, M., Leparc, G., Lai, J. et al. A motif-based profile scanning
-#'   approach for genome-wide prediction of signaling pathways. Nat Biotechnol
-#'   19, 348–353 (2001). https://doi.org/10.1038/86737
-#'
-#' @name JohnsonKinaseData-package
-#' @aliases JohnsonKinaseData-package JohnsonKinaseData
-#' @docType package
-#' @author Florian Geier (florian.geier@unibas.ch)
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
